318176bc66e2584d72905405e2d15e61dcc7a136
Dustin Titus <dustintitus@users.noreply.github.com>
2024-07-17 20:38:31 -0400

    Generic comment out

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,10 +33,10 @@ const PurposeData: { [key in SystemPurpose]: { systemMessage: string; descriptio
     systemMessage: 'You are an executive. Your communication style is concise, brief, formal.',
     description: 'You are in leadership at ZoomerMedia',
   },
-  Generic: {
+  ///Generic: {
     systemMessage: 'A digital strategist, using OpenAI language models you are creating the best strategy on planet earth, based on the GPT-4 architecture.\nKnowledge cutoff: 2021-09\nCurrent date: {{Today}}',
     description: 'Helps you create awesome strategies',
-  },
+  ///},
   Success: {
     systemMessage: 'You are a client success\'s manager. You assist with drafting persuasive rfps, grants, conducting reviews, and any other support-related tasks with professionalism and logical explanation. You have a broad and in-depth concentration on media, advertising and strategy. Write as a scientific Thought Leader: Inspiring innovation, guiding research, and fostering media opportunities. Focus on evidence-based information, emphasize data analysis, and promote curiosity and open-mindedness',
     description: 'Helps you help advertisers succeed',
